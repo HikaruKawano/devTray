80f1c59cb0dcd0c7e55182519ea78e5ed9e17586
nonanashi <nonanashi@debian.nonanashi>
2024-01-24 15:12:35 -0300

    1.0.1 - create start end stop container docker

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const { resolve, basename } = require('path');
 const { app, Tray, Menu, dialog, MenuItem } = require('electron');
 const Store = require('electron-store');
 const { spawn } = require('child_process');
+const { Docker } = require('node-docker-api');
+const path = require('path');
 
+const assetsDirectory = path.join(__dirname, 'assets');
 
 const schema = {
   projects: {
     type: 'string',
   },
+
+  dockers: {
+    type: 'string',
+  }
 }
 
 
@@ -15,13 +22,41 @@ const schema = {
 
 let mainTray = {};
 
-function render(tray = mainTray) {
+async function render(tray = mainTray) {
   const store = new Store({ schema })
+  const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
   const storedProjects = store.get('projects');
   const projects = storedProjects ? JSON.parse(storedProjects) : [];
 
-  console.log(store.get('projects'))
+
+  const container = await docker.container.list({ all: true });
+
+
+
+  const containers = container.map(container => ({
+    label: container.data.Image,
+
+    submenu: [{
+      label: `${container.data.State == 'running' ? 'Stop container' : 'start container'}`,
+      icon: path.join(
+        assetsDirectory,
+        `${container.data.State == 'running' ? 'online' : 'offline'}.png`
+      ),
+      async click() {
+        if (container.data.State === 'running') {
+          // console.log(`Stopped container ${container.data.Image}`);
+          await container.stop();
+          render();
+        } else {
+          // console.log(`Started container ${container.data.Image}`);
+          await container.start();
+          render()
+        }
+      }
+    }]
+  }))
+
 
   const items = projects.map(({ name, path }) => ({
     label: name,
@@ -44,29 +79,48 @@ function render(tray = mainTray) {
     ],
   }));
 
-  const contextMenu = Menu.buildFromTemplate([
 
+  const contextMenu = Menu.buildFromTemplate([
     {
-      type: 'separator',
+      label: 'Projects',
+      submenu: [
+        {
+          type: 'separator',
+        },
+        ...items,
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Add project', type: 'radio', click: () => {
+            dialog.showOpenDialog({ properties: ['openDirectory'] }).then(result => {
+              const [path] = result.filePaths;
+              store.set('projects', JSON.stringify([...projects, {
+                path,
+                name: basename(path),
+              }]))
+              render()
+            }).then(err => {
+              console.log(err);
+            })
+          }
+        },
+
+      ]
     },
-    ...items,
     {
-      type: 'separator',
+      label: 'Docker',
+      submenu: [
+        {
+          type: 'separator'
+        },
+        ...containers,
+        {
+          type: 'separator'
+        }
+      ]
     },
-    {
-      label: 'Add project', type: 'radio', click: () => {
-        dialog.showOpenDialog({ properties: ['openDirectory'] }).then(result => {
-          const [path] = result.filePaths;
-          store.set('projects', JSON.stringify([...projects, {
-            path,
-            name: basename(path),
-          }]))
-          render()
-        }).then(err => {
-          console.log(err);
-        })
-      }
-    },
+
     {
       type: 'normal',
       role: 'quit',
@@ -78,6 +132,8 @@ function render(tray = mainTray) {
   // contextMenu.insert(0, new MenuItem({
 
   // }));
+
+
 
   tray.setToolTip('this is my app');
   tray.setContextMenu(contextMenu);
