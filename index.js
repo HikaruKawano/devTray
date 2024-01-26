fc57f15afda9774f0432d2c46e8a0ece896af40d
nonanashi <nonanashi@debian.nonanashi>
2024-01-26 08:19:37 -0300

    auteração falha

--- index.js
+++ index.js
@@ -1,146 +1,62 @@
-const { resolve, basename } = require('path');
-const { app, Tray, Menu, dialog, MenuItem } = require('electron');
-const Store = require('electron-store');
-const { spawn } = require('child_process');
-const { Docker } = require('node-docker-api');
+const { resolve } = require('path');
+const { app, Tray, BrowserWindow } = require('electron');
+
+
+
 const path = require('path');
-
-const assetsDirectory = path.join(__dirname, 'assets');
-
-const schema = {
-  projects: {
-    type: 'string',
-  },
-
-  dockers: {
-    type: 'string',
-  }
-}
+const positioner = require('electron-traywindow-positioner');
 
 
 
 
-let mainTray = {};
 
-async function render(tray = mainTray) {
-  const store = new Store({ schema })
-  const docker = new Docker({ socketPath: '/var/run/docker.sock' });
-
-  const storedProjects = store.get('projects');
-  const projects = storedProjects ? JSON.parse(storedProjects) : [];
-
-
-  const container = await docker.container.list({ all: true });
-
-
-
-  const containers = container.map(container => ({
-    label: container.data.Image,
-
-    submenu: [{
-      label: `${container.data.State == 'running' ? 'Stop container' : 'start container'}`,
-      icon: path.join(
-        assetsDirectory,
-        `${container.data.State == 'running' ? 'online' : 'offline'}.png`
-      ),
-      async click() {
-        if (container.data.State === 'running') {
-          // console.log(`Stopped container ${container.data.Image}`);
-          await container.stop();
-          render();
-        } else {
-          // console.log(`Started container ${container.data.Image}`);
-          await container.start();
-          render()
-        }
-      }
-    }]
-  }))
-
-
-  const items = projects.map(({ name, path }) => ({
-    label: name,
-    submenu: [
-      {
-        label: 'open',
-
-        click: () => {
-          spawn('code', [path], { stdio: 'overlapped' })
-        }
-      },
-      {
-        label: 'remove',
-
-        click: () => {
-          store.set('projects', JSON.stringify(projects.filter(item => item.path !== path)));
-          render();
-        }
-      }
-    ],
-  }));
-
-
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Projects',
-      submenu: [
-        {
-          type: 'separator',
-        },
-        ...items,
-        {
-          type: 'separator',
-        },
-        {
-          label: 'Add project', type: 'radio', click: () => {
-            dialog.showOpenDialog({ properties: ['openDirectory'] }).then(result => {
-              const [path] = result.filePaths;
-              store.set('projects', JSON.stringify([...projects, {
-                path,
-                name: basename(path),
-              }]))
-              render()
-            }).then(err => {
-              console.log(err);
-            })
-          }
-        },
-
-      ]
+function createWindos() {
+  window = new BrowserWindow({
+    width: 400,
+    height: 600,
+    show: false,
+    autoHideMenuBar: true,
+    focusable: true,
+    resizable: false,
+    frame: false,
+    fullscreenable: false,
+    transparent: true,
+    movable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: true,
     },
-    {
-      label: 'Docker',
-      submenu: [
-        {
-          type: 'separator'
-        },
-        ...containers,
-        {
-          type: 'separator'
-        }
-      ]
-    },
+  })
 
-    {
-      type: 'normal',
-      role: 'quit',
-      enabled: true,
-    },
+  window.webContents.openDevTools();
+  // and load the index.html of the app.
+  window.loadFile('index.html');
+  window.on('blur', () => {
+    if (!window.webContents.isDevToolsOpened()) {
+      window.hide()
+    }
+  })
 
-  ])
+  let tray = new Tray(resolve(__dirname, 'assets', 'IconTemplate.png'));
 
-  // contextMenu.insert(0, new MenuItem({
+  const showWindow = () => {
+    positioner.position(window, tray.getBounds() - 50);
+    window.show();
+  };
 
-  // }));
+  const toggleWindow = () => {
+    if (window.isVisible()) return window.hide();
+    return showWindow();
+  };
 
 
-
+  tray.on('click', () => {
+    toggleWindow();
+  });
   tray.setToolTip('this is my app');
-  tray.setContextMenu(contextMenu);
+  tray.setContextMenu();
 }
-
 
 app.on('ready', () => {
-  mainTray = new Tray(resolve(__dirname, 'assets', 'IconTemplate.png'));
-  render(mainTray);
-})
+  createWindos()
+});
